27e882b076594aaa6969bba254886a6643d5d03e
Naromie Simeon <naromie.simeon@gmail.com>
2019-10-29 19:08:44 -0400

    Push Solution #1

---
+++ min_max.py
@@ -0,0 +1,9 @@
+the_array=[15,22,84,14,88,23]
+# for each num in range(the_array[])
+    # print(max.num)
+print(max(the_array))
+print(max(the_array))
+max_array = max(the_array)
+min_array = min(the_array)
+the_difference = max_array - min_array
+print(the_difference)
